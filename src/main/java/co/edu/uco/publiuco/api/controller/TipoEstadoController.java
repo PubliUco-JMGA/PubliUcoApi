033481e7b6941fd805e4c40040c53f7fe530c8a9
Jose Manuel <gjosemanuel469@gmail.com>
2023-05-22 11:48:48 +0100

    agregacion de clase RespuestaReporte

--- src/main/java/co/edu/uco/publiuco/api/controller/TipoEstadoController.java
+++ src/main/java/co/edu/uco/publiuco/api/controller/TipoEstadoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,16 @@ public final class TipoEstadoController {
 	@GetMapping("/{id}")
 	public TipoEstadoDTO listById(@PathVariable UUID id) {
 		return TipoEstadoDTO.create().setIdentificador(id);
+	}
+	@PostMapping
+	public ResponseEntity<Response<TipoEstadoDTO>> create(@RequestBody TipoEstadoDTO dto) {
+		List<TipoEstadoDTO> list = new ArrayList<>();
+		
+		List<String> messages = new ArrayList<>();
+		messages.add("Tipos de creados consultados exitosamente");
+		
+		Response<TipoEstadoDTO> response = new Response<>(list,messages);
+		return new ResponseEntity<>(response,HttpStatus.OK);
 	}
 	
 	
